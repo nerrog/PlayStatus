bb0034f6123527fd5e59212aba06fa021d632de6
origin <tnk@nerrog.net>
2021-01-23 13:16:19 +0900

    fix vcxproj

--- PlayStatus/Discord.cpp
+++ PlayStatus/Discord.cpp
@@ -4,6 +4,9 @@
 
 static int64_t eptime = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 
+extern char* proj_name;
+extern char* utl_ver;
+
 void Discord::Initialize()
 {
 	DiscordEventHandlers Handle;
@@ -15,10 +18,13 @@ void Discord::Update()
 {
         DiscordRichPresence discordPresence;
         memset(&discordPresence, 0, sizeof(discordPresence));
-        discordPresence.details = "Editing Movie";
+        discordPresence.state = "Editing Movie";
+        discordPresence.details = proj_name;
         discordPresence.startTimestamp = eptime;
         discordPresence.endTimestamp = NULL;
         discordPresence.largeImageKey = "icon";
         discordPresence.largeImageText = "Aviutl(Plugin by YU-PEI)";
+        discordPresence.smallImageKey = "icon";
+        discordPresence.smallImageText = utl_ver;
         Discord_UpdatePresence(&discordPresence);
 }
--- PlayStatus/main.cpp
+++ PlayStatus/main.cpp
@@ -2,32 +2,46 @@
 #include "filter.h"
 #include <Windows.h>
 
-Discord* g_Discord;
+
+Discord* g_Discord; //DiscordRPC用のポインタ
 bool inited = false;
-char* pluginname = "PlayStatus ver.0.0.1 by YU-PEI";
+char* pluginname = "PlayStatus ver.0.0.2 by YU-PEI"; //プラグイン名・バージョン
+char* proj_name; //プロジェクト名(未実装)
+char* utl_ver; //Aviutl本体のバージョン (未実装)
 
+
+// フィルタプラグイン構造体
 FILTER_DLL filter = {
-	NULL,
-	0,0,
-	pluginname,
-	NULL,
-	NULL,NULL,
-	NULL,NULL,NULL,
-	NULL,
-	NULL,
-	NULL,
-	NULL,
-	NULL,
-	NULL,NULL,
-	NULL,
-	NULL,
-	pluginname,
-	NULL,
-	NULL,
+	NULL,	// int flag
+	0, 0,	// int x,y
+	pluginname,	// TCHAR *name
+	NULL,NULL,NULL,	// int track_n, TCHAR **track_name, int *track_default
+	NULL,NULL,	// int *track_s, *track_e
+	NULL,NULL,NULL,	// int check_n, TCHAR **check_name, int *check_default
+	NULL,	// (*func_proc)
+	NULL,	// (*func_init)
+	NULL,	// (*func_exit)
+	NULL,	// (*func_update)
+	NULL,	// (*func_WndProc)
+	NULL,NULL,	// reserved
+	NULL,	// void *ex_data_ptr
+	NULL,	// int ex_data_size
+	pluginname,	// TCHAR *information
+	NULL,	// (*func_save_start)
+	NULL,	// (*func_save_end)
+	NULL,	// EXFUNC *exfunc;
+	NULL,	// HWND	hwnd;
+	NULL,	// HINSTANCE dll_hinst;
+	NULL,	// void	*ex_data_def;
+	NULL,	// (*func_is_saveframe)
+	NULL,	// (*func_project_load)
+	func_project_save,	// (*func_project_save)
+	NULL,	// (*func_modify_title)
+	NULL,	// TCHAR *dll_path;
 };
-
 void Init() {
 	//DiscordRPC初期化関数
+	//utl_ver = sip->info;
 	g_Discord->Initialize();
 	g_Discord->Update();
 
@@ -43,6 +57,10 @@ BOOL func_init(FILTER* fp) {
 	return TRUE;
 }
 
+BOOL func_project_save(FILTER* fp, void* editp, void* data, int* size) {
+	
+	return TRUE;
+}
 
 EXTERN_C FILTER_DLL __declspec(dllexport)* __stdcall GetFilterTable(void)
 {
