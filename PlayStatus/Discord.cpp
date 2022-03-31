b3c9ff37c5e1316becfb022c1ca0b4074fa7a998
nerrog <tnk@nerrog.net>
2022-04-01 01:05:09 +0900

    [Add] Aviutl本体バージョンを取得して表示

--- PlayStatus/Discord.cpp
+++ PlayStatus/Discord.cpp
@@ -2,29 +2,30 @@
 #include <time.h>
 #include <chrono>
 
+#include "filter.h"
+
 static int64_t eptime = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 
 extern char* proj_name;
-extern char* utl_ver;
+extern SYS_INFO utl_sys;
+extern char* pluginName;
 
 void Discord::Initialize()
 {
 	DiscordEventHandlers Handle;
 	memset(&Handle, 0, sizeof(Handle));
-	Discord_Initialize("KEY", &Handle, 1, NULL); //Your Api Key
+	Discord_Initialize("801042776288526349", &Handle, 1, NULL); //Default AppId
 }
 
 void Discord::Update()
 {
-        DiscordRichPresence discordPresence;
-        memset(&discordPresence, 0, sizeof(discordPresence));
-        discordPresence.state = "Editing Movie";
-        discordPresence.details = proj_name;
-        discordPresence.startTimestamp = eptime;
-        discordPresence.endTimestamp = NULL;
-        discordPresence.largeImageKey = "icon";
-        discordPresence.largeImageText = "Aviutl(Plugin by YU-PEI)";
-        discordPresence.smallImageKey = "icon";
-        discordPresence.smallImageText = utl_ver;
-        Discord_UpdatePresence(&discordPresence);
+    DiscordRichPresence discordPresence;
+    memset(&discordPresence, 0, sizeof(discordPresence));
+    discordPresence.state = "Editing Movie";
+    discordPresence.details = utl_sys.info;
+    discordPresence.startTimestamp = eptime;
+    discordPresence.endTimestamp = NULL;
+    discordPresence.largeImageKey = "icon";
+    discordPresence.largeImageText = pluginName;
+    Discord_UpdatePresence(&discordPresence);
 }
--- PlayStatus/main.cpp
+++ PlayStatus/main.cpp
@@ -2,31 +2,29 @@
 #include "filter.h"
 #include <Windows.h>
 
-
 Discord* g_Discord; //DiscordRPC用のポインタ
+SYS_INFO utl_sys;
 bool inited = false;
-char* pluginname = "PlayStatus ver.0.0.2 by YU-PEI"; //プラグイン名・バージョン
+char* pluginName = "PlayStatus 0.0.3"; //プラグイン名・バージョン
 char* proj_name; //プロジェクト名(未実装)
-char* utl_ver; //Aviutl本体のバージョン (未実装)
-
 
 // フィルタプラグイン構造体
 FILTER_DLL filter = {
 	NULL,	// int flag
 	0, 0,	// int x,y
-	pluginname,	// TCHAR *name
+	pluginName,	// TCHAR *name
 	NULL,NULL,NULL,	// int track_n, TCHAR **track_name, int *track_default
 	NULL,NULL,	// int *track_s, *track_e
 	NULL,NULL,NULL,	// int check_n, TCHAR **check_name, int *check_default
 	NULL,	// (*func_proc)
-	NULL,	// (*func_init)
+	func_init,	// (*func_init)
 	NULL,	// (*func_exit)
 	NULL,	// (*func_update)
 	NULL,	// (*func_WndProc)
 	NULL,NULL,	// reserved
 	NULL,	// void *ex_data_ptr
 	NULL,	// int ex_data_size
-	pluginname,	// TCHAR *information
+	pluginName,	// TCHAR *information
 	NULL,	// (*func_save_start)
 	NULL,	// (*func_save_end)
 	NULL,	// EXFUNC *exfunc;
@@ -35,39 +33,38 @@ FILTER_DLL filter = {
 	NULL,	// void	*ex_data_def;
 	NULL,	// (*func_is_saveframe)
 	NULL,	// (*func_project_load)
-	func_project_save,	// (*func_project_save)
+	NULL,	// (*func_project_save)
 	NULL,	// (*func_modify_title)
 	NULL,	// TCHAR *dll_path;
 };
 void Init() {
 	//DiscordRPC初期化関数
-	//utl_ver = sip->info;
 	g_Discord->Initialize();
 	g_Discord->Update();
-
 }
 
 
 BOOL func_init(FILTER* fp) {
 	//フィルタプラグイン初期化関数
 	if (!inited) {
+		//SYS_INFOの取得
+		fp->exfunc->get_sys_info(nullptr, &utl_sys);
+
+		//初期化
 		Init();
 		inited = true;
 	}
 	return TRUE;
 }
 
-BOOL func_project_save(FILTER* fp, void* editp, void* data, int* size) {
-	
-	return TRUE;
+BOOL func_exit(FILTER* fp)
+{
+	return 0;
 }
+
+
 
 EXTERN_C FILTER_DLL __declspec(dllexport)* __stdcall GetFilterTable(void)
 {
-	if (GetModuleHandleA("PlayStatus.auf") != NULL) {
-		Init();
-		inited = true;
-	}
 	return &filter;
-}
-
+} 
